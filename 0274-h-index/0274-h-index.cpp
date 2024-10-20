0f9c4cc3fdfa4f2daa26be84c22c438bbc28b4ee
Chiragjawa <124446742+Chiragjawa@users.noreply.github.com>
2024-10-21 01:17:39 +0530

    Time: 0 ms (100%), Space: 11 MB (88.58%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n= citations.size();
+        sort(citations.begin(),citations.end());
+        int hindex=0;
+        for(int i= 0;i<n;i++){
+            int h= n-i;
+            if(citations[i]>=h){
+                hindex = h;
+                break;
+            }
+        }
+        return hindex;
+    }
+};
