5c73a97551ad158c0ecdea852bed65cda34aa6a6
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2025-08-30 00:06:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.22%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long n_ceil = (n + 1) / 2; // ceil(n/2)
-        long long n_floor = n / 2;      // floor(n/2)
-        long long m_ceil = (m + 1) / 2; // ceil(m/2)
-        long long m_floor = m / 2;      // floor(m/2)
-
-        return n_ceil * m_floor + n_floor * m_ceil;
+        return (long long)(((long long)(n + 1) / 2) *(long long) (m / 2) )+((long long)(n / 2) *( (long long)(m + 1) / 2));
     }
 };
