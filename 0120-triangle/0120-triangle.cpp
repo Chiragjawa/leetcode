d30e6c0176152bd873ca47634905fd4df19a4e99
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2025-09-25 10:28:08 +0530

    Time: 0 ms (100%), Space: 13.3 MB (55.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,32 @@
 class Solution {
 private:
-	int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
-		if(i == n - 1)
-			return triangle[i][j];
-		if(dp[i][j] != -1)
-			return dp[i][j];
-		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
-		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
-		return dp[i][j] = min(bottom, bottom_right);
-	}
+    // int func(int i , int j , vector<vector<int>>& triangle , vector<vector<int>>&dp){
+    //     if(i== triangle.size()){
+    //         return triangle[i][j];
+    //     }
+    //     if(dp[i][j] != -1) return dp[i][j];
+        
+    //         int first = func(i+1 , j , triangle ,dp);
+    //         int second = func(i+1 , j+1 , triangle ,dp);
+        
+
+    //     return dp[i][j] = min(first, second) + triangle[i][j];
+    // }
 public:
-int minimumTotal(vector<vector<int>>& triangle) {
-		int n = triangle.size();
-		vector<vector<int>>dp(n, vector<int>(n, -1));
-		return solve(0, 0, n, triangle, dp);
-	}
-	
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        vector<vector<int>>dp(m, vector<int>(m,-1));
+        for(int i =0 ;i<triangle.size();i++){
+            dp[m-1][i] = triangle[m-1][i];
+        }
+        for(int i =m-2 ;i>=0 ;i--){
+            for(int j = i; j >= 0; j--) {
+				int up = triangle[i][j] + dp[i+1][j];
+				int up_left = triangle[i][j] + dp[i+1][j+1];
+				dp[i][j] = min(up, up_left);
+			}
+        }
+        return dp[0][0];
+      
+    }
 };
