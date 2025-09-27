df7d5d470ebe5655a6a1a752a3003759ae3928dc
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2025-09-27 16:36:52 +0530

    Time: 22 ms (47.64%), Space: 28 MB (69.54%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -16,9 +16,9 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1 ,0));
-        // for(int i=0;i<n;i++) {
-        //     dp[i][0] == 1;
-        // }
+        for(int i=0;i<n;i++) {
+            dp[i][0] == 1;
+        }
         for(int t = 0; t <= amount; ++t) {
             if(t % coins[0] == 0)
                 dp[0][t] = 1;
