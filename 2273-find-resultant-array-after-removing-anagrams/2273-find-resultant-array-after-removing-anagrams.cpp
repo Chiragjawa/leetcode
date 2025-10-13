c9e6ba7e0b624918facdc980a888d701b56eebfe
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2025-10-13 12:03:16 +0530

    Time: 7 ms (19.08%), Space: 16 MB (75.29%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string>ans;
+        ans.push_back(words[0]);
+        for(int i=1;i<words.size();i++){
+            string a = words[i]; string b=ans.back();
+            sort(a.begin(),a.end());
+            sort(b.begin(), b.end());
+            if(a!=b) ans.push_back(words[i]);
+            else{
+                continue;
+            }
+        }
+        return ans;
+    }
+};
