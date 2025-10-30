c03626711259ea558693d159958771bca2e628c7
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2025-10-30 13:24:18 +0530

    Time: 0 ms (100%), Space: 28.2 MB (63.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if(nums.size()==1){
-           return nums[0];
-       }
-        int compare = nums.size()/2;
-        int cnt =1;
-        int answer = 0;
-        for(int i=0 ;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]){
-                cnt++;
-                if(cnt>compare){
-                    answer = nums[i];
-                }
+        int count=0;
+        int number=0;
+        for(int i=0;i<nums.size();i++){
+            if(count ==0){
+                number = nums[i];
+                count++;
             }
-            else{
-                cnt =1;
+            else if(number == nums[i]){
+                count++;
             }
-
+            else {
+                count--;
+            }
         }
-        return answer;
-    }   
+        return number;
+    }
 };
