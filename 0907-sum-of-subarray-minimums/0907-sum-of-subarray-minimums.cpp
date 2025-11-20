5130434a3c6edd8c41ec79683dd27020137b9499
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2025-11-20 17:22:05 +0530

    Time: 11 ms (87.98%), Space: 50.2 MB (6.56%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,22 +1,25 @@
 class Solution {
-private:
-    vector<int> findnse(vector<int>&arr){
-        vector<int>nse(arr.size());
-        stack<int>st;
-        for(int i= arr.size()-1 ;i>=0 ;i--){
-            while(!st.empty() && arr[st.top()]>=arr[i]){
+    vector<int>findnse(vector<int>arr){
+        stack<int> st;
+        int n = arr.size();
+        vector<int>nse(n,0);
+
+        for(int i = n-1; i>=0;i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
                 st.pop();
             }
-            nse[i] = st.empty() ? arr.size() : st.top();
+            nse[i] = st.empty() ? n : st.top();
             st.push(i);
         }
         return nse;
     }
-    vector<int> findpse(vector<int>&arr){
-        vector<int>pse(arr.size());
-        stack<int>st;
-        for(int i=0 ;i<arr.size() ;i++){
-            while(!st.empty() && arr[st.top()]>arr[i]){
+    vector<int>findpse(vector<int>arr){
+        stack<int> st;
+        int n = arr.size();
+        vector<int>pse(n,0);
+
+        for(int i =0; i<n;i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
                 st.pop();
             }
             pse[i] = st.empty() ? -1 : st.top();
@@ -28,12 +31,12 @@ public:
     int sumSubarrayMins(vector<int>& arr) {
         vector<int>nse = findnse(arr);
         vector<int>pse = findpse(arr);
-        long long total=0;
-       int mod = 1e9 + 7;
-        for(int i=0 ;i<arr.size();i++){
+        int total =0;
+        int mod = 1e9+7;
+        for(int i =0;i<arr.size();i++){
             int left = i - pse[i];
             int right = nse[i] -i;
-            total = (total + ( (long long)right * left * arr[i])%mod)%mod;
+            total = (total +((long long) left* right* arr[i]) %mod)%mod;
         }
         return total;
     }
