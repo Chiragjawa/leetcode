8eb442ab936e2ada0502f00b70d1c346e44f2599
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2026-01-05 21:26:55 +0530

    Time: 598 ms (5.19%), Space: 38.9 MB (99.53%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int cnt=0;
+        int cnt2=0;
+        int mini =INT_MAX;
+        int  n= matrix.size();
+        for(int i =0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]<0){
+                    cnt++;
+                    cout<<mini<<endl;
+                }
+                if(matrix[i][j] ==0){
+                    cnt2++;
+                }
+                mini = min(mini,abs(matrix[i][j]));
+            }
+        }
+        long long sum =0;
+         for(int i =0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sum += abs(matrix[i][j]);
+            }
+        }
+        if(cnt % 2 == 0) return sum;
+        else if((cnt % 2 != 0)  &&  cnt2 !=0) return sum;
+
+        return (long long)sum - (2*mini);
+    }
+};
