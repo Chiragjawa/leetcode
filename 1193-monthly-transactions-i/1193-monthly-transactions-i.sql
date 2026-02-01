3fe2618dfb9d483ea15223a2ebfa875aeb386dab
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2026-02-01 22:20:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT CONVERT(char(7), trans_date, 120) month, country, count(1) trans_count,
+    sum(case when state = 'approved' then 1 else 0 end) approved_count,
+    sum(amount) trans_total_amount,
+    sum(case when state = 'approved' then amount else 0 end) approved_total_amount
+from Transactions
+group by CONVERT(char(7), trans_date, 120), country;
