93c62845d1ac5c2e345fc4c5ff02f7107d3477ad
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2026-02-12 21:36:12 +0530

    Time: 219 ms (35.47%), Space: 26.9 MB (32.42%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int solve(unordered_map<char,int> &mp){
-        int mini = INT_MAX, maxi = 0;
-        for (auto &p : mp) {
-            mini = min(mini, p.second);
-            maxi = max(maxi, p.second);
-        }
-        return mini == maxi;
-    }
+ bool valid(unordered_map<char, int>& mp){
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
 
+        for(auto &it: mp){
+            maxi = max(maxi, it.second);
+            mini = min(mini, it.second);
+        }
+        if(maxi == mini) return true;
+        return false;
+    }
     int longestBalanced(string s) {
         int n = s.size();
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            unordered_map<char,int> mp;  
-            for (int j = i; j < n; j++) {
+        unordered_map<char, int> mp;
+        int ans=0;
+        for(int i =0;i<n;i++){
+            mp.clear();
+            for(int j=i;j<n;j++){
                 mp[s[j]]++;
-                if (solve(mp)) {
-                    int l = j - i + 1;
-                    ans = max(ans, l);
+                if(valid(mp)){
+                    ans = max(ans, j-i+1);
                 }
             }
         }
         return ans;
     }
+   
 };
