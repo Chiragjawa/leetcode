0db2cdca42a1dff2216adc08a01365a6410ebae0
Chirag Jawa <124446742+Chiragjawa@users.noreply.github.com>
2026-02-15 18:47:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (59.48%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,21 +1,35 @@
 class Solution {
- public:
-  string addBinary(string a, string b) {
-    string ans;
-    int carry = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
+public:
+    string addBinary(string a, string b) {
+        char carr = '0';
+        string sol = "";
+        int n = a.size() - 1;
+        int m = b.size() - 1;
 
-    while (i >= 0 || j >= 0 || carry) {
-      if (i >= 0)
-        carry += a[i--] - '0';
-      if (j >= 0)
-        carry += b[j--] - '0';
-      ans += carry % 2 + '0';
-      carry /= 2;
+        while (n >= 0 || m >= 0) {
+            char x = (n >= 0) ? a[n] : '0';
+            char y = (m >= 0) ? b[m] : '0';
+
+            int sum = (x - '0') + (y - '0') + (carr - '0');
+
+            if (sum % 2 == 0)
+                sol += '0';
+            else
+                sol += '1';
+
+            if (sum >= 2)
+                carr = '1';
+            else
+                carr = '0';
+
+            n--;
+            m--;
+        }
+
+        if (carr == '1')
+            sol += '1';
+
+        reverse(sol.begin(), sol.end());
+        return sol;
     }
-
-    reverse(begin(ans), end(ans));
-    return ans;
-  }
 };
